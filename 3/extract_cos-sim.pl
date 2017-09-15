5c24b1861bb4796612984f62fd7f87b103bcf00d
bonohu <bonohu@gmail.com>
2017-09-15 11:05:58 +0900

    Create extract_cos-sim.pl
    Extract cos-sim values from the matrix.

---
+++ 3/extract_cos-sim.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+# simple code to extract cos-sim values 
+# usage: cat result_sample.txt| perl extract_xRR.pl SRR771372 |sort -k2 -rn|less
+
+my $id = shift(@ARGV);
+my $i = 0;
+
+$line = <STDIN>;
+foreach my $srrid (split(/\t/,$line)) {
+        if($srrid eq $id) {
+                #print "$i\t$id\n";
+                last;
+        }
+        $i++;
+}
+while(<STDIN>) {
+        chomp;
+        my($srrid,@value) = split(/\t/);
+        print "$srrid\t$value[$i-1]\n";
+}
