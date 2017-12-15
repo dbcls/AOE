2732edd5adb3cd329c275256d0277ee9ae218fd0
bonohu <bonohu@gmail.com>
2017-12-15 18:39:10 +0900

    Update 00getprojsample.sh

--- 00getprojsample.sh
+++ 00getprojsample.sh
@@ -2,11 +2,5 @@
 # script to get bioproject to GSE
 
 # scraping
-perl 00getprojsample.pl #scraping
-# fix line break
-perl -pe 's/\{\"Package/\n\{\"Package/g' prj2gse.json \
-#
-pigz prj2gse.json
-
-# old code
-# perl 00parseprjgse.pl |  pigz -c > PRJ2GSE.txt.gz
+perl 00getprojsample.pl \
+| pigz -c prj2gse.json.gz
