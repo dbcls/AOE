4db4071027ecedf50bef0a8d3e8d914bad138228
bonohu <bonohu@gmail.com>
2017-12-15 18:39:48 +0900

    Update 00getlistofxRX.pl

--- 00getlistofxRX.pl
+++ 00getlistofxRX.pl
@@ -6,28 +6,31 @@ my $apip = "IP.txt";
 
 open(FILE, "$apip") or die "Cannot open $apip\n";
 while(<FILE>) {
-	chomp;
-	$ip = $_;
+        chomp;
+        $ip = $_;
 }
 close FILE;
 
 # counting the number of rows
 open(FILE, "curl \"http://$ip/api/sra/experiment?library_strategy=RNA-Seq\" |") or die;
 while(<FILE>) {
-	chomp;
-	$num = $1 if(/(\d+) items/);
+        chomp;
+        $num = $1 if(/(\d+) items/);
 }
 close FILE;
 
 # calculating interation number
 my $num2 = int ($num/1000);
-print "Iteration: $num2\n";
+print STDERR "Iteration: $num2\n";
 
-foreach my $i (0..$num2) { # modify the number 
-	$start = $i*$rows;
-	print STDERR "$i..";
-	system("curl \"http://$ip/api/sra/experiment?library_strategy=RNA-Seq&start=$start&rows=$rows&data_type=full\" >> xRX.json");
-	sleep 1;
+foreach my $i (0..$num2) { # modify the number
+        $start = $i*$rows;
+        print STDERR "$i..";
+        open(FILE, "curl -s \"http://$ip/api/sra/experiment?library_strategy=RNA-Seq&start=$start&rows=$rows&data_type=full\" |") or die;
+        while(<FILE>) {
+                s/\{\"EXPERIMENT\"/\n\{\"EXPERIMENT\"/g;
+                print "$_\n";
+        }
+        close FILE;
+        sleep 1;
 }
-# after the scraping, '\n' should be inserted by running following command
-# perl -i~ -pe 's/\{\"EXPERIMENT\"/\n\{\"EXPERIMENT\"/g' xRX.json
