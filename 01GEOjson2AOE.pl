acb7eced61cab32676d8f6bfdb43bb53738aa5d5
Hidemasa Bono <bonohu@gmail.com>
2018-04-02 20:53:58 +0900

    bug fixed

--- 01GEOjson2AOE.pl
+++ 01GEOjson2AOE.pl
@@ -7,11 +7,11 @@ while(<STDIN>) {
 	chomp;
 	my $desc = $1 if(/\"Description\":\s+\"([^\"]+)\"/);
 	my $pmid = $1 if(/\"PMID\":\s+\"([^\"]+)\"/);
-	my $id   = $1 if(/\"ID\":\s+\"([^\"]+)\"/);
-	my $title = $1 if(/\"Title\":\s+\"([^\"]+)\"/);
-	my $organismname = $1 if(/\"OrganismName\":\s+\"([^\"]+)\"/);
+	my $id   = $1 if(/\"ID\":\s+\{\"\$\":\s+\"([^\"]+)\"/); 
+	my $title = $1 if(/\"Title\":\s+\{\"\$\":\s+\"([^\"]+)\"/);
+	my $organismname = $1 if(/\"OrganismName\":\s+\{\"\$\":\s+\"([^\"]+)\"/);
 	my $submitted = $1 if(/\"\@submitted\":\s+\"([^\"]+)\"/);
 	my $method_type = $1 if(/\"\@method_type\":\s+\"([^\"]+)\"/);
-	my $datatype = $1 if(/\"\DataType\":\s+\"([^\"]+)\"/);
+	my $datatype = $1 if(/\"\DataType\":\s+\{\"\$\":\s+\"([^\"]+)\"/);
 	print "$id\t$organismname\t$title\t$method_type\t$datatype\t$submitted\n";
 }
